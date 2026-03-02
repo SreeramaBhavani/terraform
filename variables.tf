6263d08e7a1c83bd72c1c6aa82542e30be8a4b2d
SreeramaBhavani <bhavanisreerama@gmail.com>
2026-03-02 17:36:07 +0530

    Adding new file

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "filename"{
-      
+    default = "Pets.txt"
 }
 variable "content" {
-   
+    default = "I Love Rabbits!"
 }
 variable "prefix" {
-    
+    default = "MR"
 }   
 variable "separator" {
-
+    default = "&"
 }
 variable "length" {
-    
+    default = "2"
 }
